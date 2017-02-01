059a630913138bf71f861eaa940147340199280a
Martinkevich Alexander <martin@Martin-iMac.local>
2017-02-01 17:40:27 +0300

    Add oh-my-zsh support

--- .zshrc
+++ .zshrc
@@ -1,5 +1,5 @@
 # Path to your oh-my-zsh installation.
-export ZSH=$HOME/.oh-my-zsh
+export ZSH=$HOME/.dotfiles/oh-my-zsh
 
 # Set name of the theme to load.
 # Look in ~/.oh-my-zsh/themes/
@@ -81,4 +81,4 @@ source $ZSH/oh-my-zsh.sh
 # alias ohmyzsh="mate ~/.oh-my-zsh"
 
 # Add env.sh
-. ~/Projects/config/env.sh
+. $HOME/.dotfiles/config/env.sh
---
+++ config/env.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+# PATH
+export PATH=$PATH:$HOME/bin:$HOME/.composer/vendor/bin
+export EDITOR='subl -w'
+# export PYTHONPATH=$PYTHONPATH
+# export MANPATH="/usr/local/man:$MANPATH"
+
+# Virtual Environment
+#export WORKON_HOME=$HOME/.virtualenvs
+#export PROJECT_HOME=$HOME/Projects
+#source $HOME/Library/Python/2.7/bin/virtualenvwrapper.sh
+
+# Owner
+export USER_NAME=$(whoami)
+eval "$(rbenv init -)"
+
+# Application settings
+export HOMEBREW_CASK_OPTS="--appdir=~/Applications"
+
+# FileSearch
+function f() { find . -iname "*$1*" ${@:2} }
+function r() { grep "$1" ${@:2} -R . }
+
+#mkdir and cd
+function mkcd() { mkdir -p "$@" && cd "$_"; }
+
+# Aliases
